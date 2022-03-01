f5bb02245198d85d64602c9fbd4d1fcf50c2d09f
mykhailo <m.diakovych13@gmail.com>
2022-03-01 21:46:25 +0200

    Added header

--- components/general/Header.vue
+++ components/general/Header.vue
@@ -1,38 +1,31 @@
 <template>
-	<div class="navbar__wrapper d-flex flex-column flex-md-row align-items-center justify-content-between justify-content-md-between overflow-hidden mh-100 position-relative">
-		<b-navbar class="navbar__wrapper__content p-0 d-flex justify-content-md-between z-index-1 m-0 w-100" toggleable="lg" type="dark" variant="">
+	<div class="pt-4 d-flex flex-column flex-md-row align-items-center justify-content-between justify-content-md-between overflow-hidden position-relative">
+		<b-navbar class="header p-0 pb-4 z-index-1 m-0 w-100 d-flex align-items-center" toggleable="lg" type="dark" variant="">
 			<b-navbar-brand to="/preview">
-				<div class="mr-xl-5">
-<!--					<b-img class="navbar__wrapper__content-logo" src="@/assets/img/logos/metazoid_social_club_logo.svg"/>-->
+				<div class="header__brand mr-xl-5">
+					NFT UA
 				</div>
 			</b-navbar-brand>
 			<b-navbar-toggle target="nav-collapse" class='order-2'></b-navbar-toggle>
-			<b-collapse id="nav-collapse" class="order-md-1 order-2 justify-content-center justify-content-md-center" is-nav>
-				<b-navbar-nav class="navbar__wrapper__content__links d-flex justify-content-center align-items-center">
-					<b-nav-item class="navbar__wrapper__content__links-link shadow-none text-nowrap" to="/#home">HOME</b-nav-item>
-					<b-nav-item class="navbar__wrapper__content__links-link shadow-none text-nowrap" to="/#about-project">ABOUT PROJECT</b-nav-item>
-					<b-nav-item class="navbar__wrapper__content__links-link shadow-none text-nowrap" to="/#where-buy">WHERE TO BUY</b-nav-item>
-					<b-nav-item class="navbar__wrapper__content__links-link shadow-none text-nowrap" to="/#road-map">ROAD MAP</b-nav-item>
-					<b-nav-item class="navbar__wrapper__content__links-link shadow-none text-nowrap" to="/#our-team">OUR TEAM</b-nav-item>
-					<b-nav-item class="navbar__wrapper__content__links-link shadow-none text-nowrap mb-3 mb-lg-0" to="/#members">MEMBERS</b-nav-item>
+			<b-collapse id="nav-collapse" class="order-md-1 order-2 justify-content-between align-items-center" is-nav>
+				<b-navbar-nav class=" d-flex justify-content-center align-items-center">
+					<b-nav-item class="header__link shadow-none text-nowrap" to="/#home">Collection</b-nav-item>
+					<b-nav-item class="header__link shadow-none text-nowrap" to="/#about-project">Our mission</b-nav-item>
+					<b-nav-item class="header__link shadow-none text-nowrap" to="/#where-buy">Our team</b-nav-item>
+					<b-nav-item class="header__link shadow-none text-nowrap" to="/#road-map">Roadmap </b-nav-item>
+					<b-nav-item class="header__link shadow-none text-nowrap" to="/#our-team">FAQ</b-nav-item>
 				</b-navbar-nav>
+				<div class="d-flex justify-content-center z-index-1 order-1">
+					<b-button class="brand-button d-flex align-items-center justify-content-center">Partner with us</b-button>
+				</div>
 			</b-collapse>
-			<div class="navbar__wrapper__content__socials d-flex justify-content-center z-index-1 order-1">
-				<b-link class="navbar__wrapper__content__socials-link d-flex align-items-center justify-content-center rounded-circle m-2">
-<!--					<b-img class="align-items-center" src="@/assets/img/social-icon/discord_green.svg"/>-->
-				</b-link>
-				<b-link class="navbar__wrapper__content__socials-link d-flex align-items-center justify-content-center rounded-circle ml-md-4 m-2">
-<!--					<b-img src="@/assets/img/social-icon/twitter_green.svg"/>-->
-				</b-link>
-				<b-link class="navbar__wrapper__content__socials-link d-flex align-items-center justify-content-center rounded-circle ml-md-4 m-2">
-<!--					<b-img src="@/assets/img/social-icon/instagram_green.svg"/>-->
-				</b-link>
-			</div>
 		</b-navbar>
 	</div>
 </template>
 
 <script>
+import {BCollapse, BLink, BNavbar, BNavbarBrand, BNavItem} from "bootstrap-vue";
+
 export default {
 	methods: {
 		async onWalletConnect() {
@@ -50,51 +43,21 @@ export default {
 }
 </script>
 <style lang="scss">
-//@import "../../assets/scss/variables";
-//@import "../../assets/scss/mixins";
-.navbar__wrapper {
-	&__content {
-		.navbar-brand {
-			outline: none !important;
-		}
-		&__links {
-			&-link {
-				//@include fonts(normal, 400, clamp(.4rem,.7vw,.625rem), ('Press Start 2P', cursive) );
-				line-height: 1rem;
-				text-align: center;
-				margin: 0 1rem;
-				color: #96939D !important;
-				position: relative;
-				.nav-link:focus {
-					outline: none !important;
-				}
-				&:hover {
-					color: #fff;
+@import "assets/scss/main";
+.header {
+	border-bottom: $dark 2px solid;
 
-					&::before {
-						content: '';
-						width: 100%;
-						height: 0.125rem;
-						//background-color: $background-btn;
-						position: absolute;
-						left: 0;
-						top: 2rem;
-					}
-				}
-			}
+	&__link {
+		.nav-link {
+			color: $dark !important;
+			line-height: 1.2rem;
+			@include fonts(normal, 300, 1.25rem, "Neue Machina")
 		}
-		&__socials {
-			&-link {
-				width: 2.5rem;
-				height: 2.5rem;
-				//border: 0.0625rem solid $background-btn;
-
-				&:hover {
-					background-color: rgba(255,255,255,.1);
-					border: 0.0625rem solid rgba(131, 255, 140, 0.62);
-				}
-			}
-		}
+	}
+	&__brand {
+		color: $dark !important;
+		line-height: 2.3rem;
+		@include fonts(normal, 800, 2.5rem, "Neue Machina-bold")
 	}
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class='home-bg-frame'>
-		<Header/>
+		<Header class="nft-container"/>
 		<Nuxt />
 		<Footer/>
 	</div>
