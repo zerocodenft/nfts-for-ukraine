4215121e568410877f070cfea806100b0de49a08
Ihor Bodnarchuk <ihorbond@gmail.com>
2022-03-16 22:52:08 -0500

    update smart contract

--- contracts/APEUA.sol
+++ contracts/APEUA.sol
@@ -17,9 +17,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Base64.sol";
 
 contract ApeInForUkraine is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
+    using Strings for uint;
     Counters.Counter private _tokenIds;
 
     uint public constant TOKENS_PER_TRAN_LIMIT = 10;
@@ -32,18 +34,19 @@ contract ApeInForUkraine is ERC721URIStorage, Ownable {
     constructor(address beneficiary_) 
     ERC721("ApeInForUkraine", "APEUA"){
         _beneficiary = beneficiary_;
-
-        _collections[1] = Collection(1, 0.015 ether, 1000, "ipfs://QmNSwrTn4UmTe3p3hYnPV9m3UjWMm6ZDcuXFu7MYdrjd1e");
-        _collections[2] = Collection(2, 0.03 ether, 1000, "ipfs://QmeESeH2y7RhcUf9CtMcfC59DixZV9qqUz9msQB9smV1oD");
-        _collections[3] = Collection(3, 0.03 ether, 1000, "ipfs://QmULvuSaCDxVQHTZJwwSKwY8MgU49HycTub8y28qrpV3Sy");
-        _collections[4] = Collection(4, 0.03 ether, 1000, "ipfs://QmVhJAQfLbTKBCVfqsdHDo7tTqyYWtYGBrnyjdsSEcYVee");
+        _collections[1] = Collection(1, 0.015 ether, 1000, "Remarkable Ukrainian",  "ipfs://QmetKB1nPZJUV78KTu2gnWvCSDnLvwreWZuVRvQfngesnn");
+        _collections[2] = Collection(2, 0.03 ether, 1000, "World of Ukrainian Women", "ipfs://QmPAHNweTECJDKj72VdqaxwAARkuXGirXrgP1Rq7dk4bSr");
+        _collections[3] = Collection(3, 0.03 ether, 1000, "Ukrainian Gopnik", "ipfs://QmctC5DHb64qNvybuDToWHY7xxSpnL1GQ92xmpbSBDJTBb");
+        _collections[4] = Collection(4, 0.03 ether, 1000, "Torrent", "ipfs://QmYvmQrq3dnD8b5VenxBtU8ySG3pY3wcksGbSK1AgivsbX");
+        _collections[5] = Collection(5, 0.06 ether, 300, "Zelensky Punk", "ipfs://QmStvhVzbV17DBe6XCmdh4E3Hr5KnKWC7r3LVqvddPq86x");
     }
 
     struct Collection {
         uint id;
         uint price;
         uint size;
-        string uri;
+        string name;
+        string imageUri;
     }
 
     function totalSupply() external view returns (uint) {
@@ -77,7 +80,7 @@ contract ApeInForUkraine is ERC721URIStorage, Ownable {
         Collection memory collection = _collections[collectionId];
         require(collection.id != 0, "APEUA: Invalid collectionId");
         require(_tokenIds.current() + count <= collection.size, "Request exceeds collection size");
-        _mintTokens(to, count, collection.uri);
+        _mintTokens(to, count, collection);
     }
     
     function mint(uint count, uint collectionId) external payable {
@@ -90,20 +93,41 @@ contract ApeInForUkraine is ERC721URIStorage, Ownable {
         require(_collectionMintedCount[collectionId] + count <= collection.size, "APEUA: Number of requested tokens will exceed limit");
         require(msg.value >= count * collection.price, "APEUA: Ether value sent is not sufficient");
 
-        _collectionMintedCount[collectionId] += count;
-        _mintTokens(msg.sender, count, collection.uri);
+        _mintTokens(msg.sender, count, collection);
     }
 
     /// @dev Perform actual minting of the tokens
-    function _mintTokens(address to, uint count, string memory tokenUri) internal {
+    function _mintTokens(address to, uint count, Collection memory collection) internal {
         for(uint index = 0; index < count; index++) {
 
             _tokenIds.increment();
             uint newItemId = _tokenIds.current();
 
+            uint nextInCollection = _collectionMintedCount[collection.id];
+            string memory name = string(abi.encodePacked(collection.name, " #", (nextInCollection+1).toString()));
+            string memory tokenUri = formatTokenURI(collection.imageUri, name);
+
+            _collectionMintedCount[collection.id] += 1;
             _safeMint(to, newItemId);
             _setTokenURI(newItemId, tokenUri);
         }
+    }
+
+    function formatTokenURI(string memory _imageURI, string memory _name) private pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "data:application/json;base64,",
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"', _name,
+                            '", "description":"Join people around the world in using the power of cryptocurrency to undermine Putin\'s efforts of destruction"',
+                            ', "image":"', _imageURI, '"}'
+                        )
+                    )
+                )
+            )
+        );
     }
 
 }
